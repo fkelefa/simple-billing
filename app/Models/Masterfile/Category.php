f764e99f64ca58315ade7e3632f23770096edf22
F Kelefa <f_kelefa@hotmail.com>
2023-03-12 18:35:20 +0000

    add model and migration foe the remaing files Item, ctegory and Salesman

---
+++ app/Models/Masterfile/Category.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models\Masterfile;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    protected $table = 'category';
+
+    protected $fillable = [
+        'code',
+        'name',
+    ];
+}
